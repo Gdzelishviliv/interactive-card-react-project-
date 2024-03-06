d2f84a288fdc7a6f2ba4fd6ade74924d96d431fa
Ivane <gdzelishviliv@gmail.com>
2024-03-06 18:11:03 +0400

    added confirmation btn

--- src/components/inputs/Intputs.jsx
+++ src/components/inputs/Intputs.jsx
@@ -9,18 +9,19 @@ const Intputs = () => {
       <label htmlFor="Card Number">Card Number</label>
       <input type="number" placeholder="e.g. 1234 5678 9123 0000" />
       <div className="exp-date-cvc">
-            <div className="month-year">
-                <label htmlFor="Exp. Date (MM/YY)">Exp. Date (MM/YY)</label>
-                <div className="mm-yy">
-                    <input type="number" placeholder="MM" />
-                    <input type="number" placeholder="YY" />
-                </div>
-            </div>
-            <div className="cvc-div">
-                <label htmlFor="CVC">CVC</label>
-                <input type="number" placeholder="e.g. 123" id="cvc-inp"/>
-            </div>
+        <div className="month-year">
+          <label htmlFor="Exp. Date (MM/YY)">Exp. Date (MM/YY)</label>
+          <div className="mm-yy">
+            <input type="number" placeholder="MM" />
+            <input type="number" placeholder="YY" />
+          </div>
+        </div>
+        <div className="cvc-div">
+          <label htmlFor="CVC">CVC</label>
+          <input type="number" placeholder="e.g. 123" id="cvc-inp" />
+        </div>
       </div>
+      <button className="confirm-btn">Confirm</button>
     </div>
   );
 };
