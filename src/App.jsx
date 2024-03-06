7f5ea26b5a752e26252a4e58f2b4c4ef0f553642
Ivane <gdzelishviliv@gmail.com>
2024-03-06 18:08:01 +0400

    finished mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Card from "./components/cards/Card";
+import Intputs from "./components/inputs/Intputs";
 
 function App() {
   return <>
   <Card/>
-  
+  <Intputs/>
   </>;
 }
 
---
+++ src/components/inputs/Intputs.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./inputs.css";
+
+const Intputs = () => {
+  return (
+    <div className="inputs-container">
+      <label htmlFor="Cardholder Name">Cardholder Name</label>
+      <input type="text" placeholder="e.g. Jane Appleseed" />
+      <label htmlFor="Card Number">Card Number</label>
+      <input type="number" placeholder="e.g. 1234 5678 9123 0000" />
+      <div className="exp-date-cvc">
+            <div className="month-year">
+                <label htmlFor="Exp. Date (MM/YY)">Exp. Date (MM/YY)</label>
+                <div className="mm-yy">
+                    <input type="number" placeholder="MM" />
+                    <input type="number" placeholder="YY" />
+                </div>
+            </div>
+            <div className="cvc-div">
+                <label htmlFor="CVC">CVC</label>
+                <input type="number" placeholder="e.g. 123" id="cvc-inp"/>
+            </div>
+      </div>
+    </div>
+  );
+};
+
+export default Intputs;
